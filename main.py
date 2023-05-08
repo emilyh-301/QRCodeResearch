70089459485134dc5ff2706c0e5b1da3e4d64a75
emilyh-301 <36495239+emilyh-301@users.noreply.github.com>
2023-05-08 19:22:42 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import qrcode.image.svg
+import qrcode
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data('Some data')
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+
+img = qr.make_image(back_color=(255, 195, 235), fill_color=(55, 95, 35))
+
+method = 'basic'
+if method == 'basic':
+    # Simple factory, just a set of rects.
+    factory = qrcode.image.svg.SvgImage
+elif method == 'fragment':
+    # Fragment factory (also just a set of rects)
+    factory = qrcode.image.svg.SvgFragmentImage
+else:
+    # Combined path factory, fixes white space that may occur when zooming
+    factory = qrcode.image.svg.SvgPathImage
+
+img = qrcode.make('Some data here', image_factory=factory)
