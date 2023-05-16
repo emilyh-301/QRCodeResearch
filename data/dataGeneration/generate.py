e70076ae60760b5393fecd85d1f86c6b802fbc38
emilyh-301 <36495239+emilyh-301@users.noreply.github.com>
2023-05-15 20:28:15 -0400

    generate some data

---
+++ data/dataGeneration/generate.py
@@ -0,0 +1,59 @@
+import numpy as np
+import qrcode
+import secrets
+from functools import partial
+
+alphanumeric = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+
+# 20,000 total
+# 16,000 train
+#  4,000 test
+def produce_amount_keys(amount_of_keys, length=30):
+    keys = set()
+    pickchar = partial(secrets.choice, alphanumeric)
+    while len(keys) < amount_of_keys:
+        keys |= {''.join([pickchar() for _ in range(length)]) for _ in range(amount_of_keys - len(keys))}
+    train = open('../train/queryStrings.txt', 'a')
+    test = open('../text/queryStrings.txt', 'a')
+    count = 0
+    for key in keys:
+        if count % 5 == 0:
+            test.write(key + '\n')
+        else:
+            train.write((key + '\n'))
+        count += 1
+    train.close()
+    test.close()
+
+# simplest QR Code with no border and box_size of 1
+qr = qrcode.QRCode(
+    version=1,
+    box_size=1,
+    border=0,
+)
+qr.make(fit=True)
+
+trainStrings = open('../train/queryStrings.txt', 'r')
+trainCodes = open('../train/qrCodes.txt', 'a')
+while True:
+    line = trainStrings.readline()
+    if not line:
+        break
+    qr.add_data('https://h3turing.vmhost.psu.edu?' + line)
+    matrix = qr.get_matrix()
+    np.savetxt(trainCodes, np.asarray(matrix))
+trainCodes.close()
+trainStrings.close()
+
+testStrings = open('../test/queryStrings.txt', 'r')
+testCodes = open('../test/qrCodes.txt', 'a')
+while True:
+    line = testStrings.readline()
+    if not line:
+        break
+    qr.add_data('https://h3turing.vmhost.psu.edu?' + line)
+    matrix = qr.get_matrix()
+    np.savetxt(testCodes, np.asarray(matrix))
+testCodes.close()
+testStrings.close()
+
--- main.py
+++ main.py
@@ -1,28 +1,41 @@
 import qrcode.image.svg
 import qrcode
+from PIL import Image
 
 qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
     box_size=10,
-    border=4,
+    border=0,
 )
-qr.add_data('Some data')
+qr.add_data('https://h3turing.vmhost.psu.edu?Abcdefghij1234567890abcdefghiJ')
 qr.make(fit=True)
 
+print(qr.get_matrix())
+
+
+alphanumeric = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+
 img = qr.make_image(fill_color="black", back_color="white")
+# img.show()
 
-img = qr.make_image(back_color=(255, 195, 235), fill_color=(55, 95, 35))
 
-method = 'basic'
-if method == 'basic':
-    # Simple factory, just a set of rects.
-    factory = qrcode.image.svg.SvgImage
-elif method == 'fragment':
-    # Fragment factory (also just a set of rects)
-    factory = qrcode.image.svg.SvgFragmentImage
-else:
-    # Combined path factory, fixes white space that may occur when zooming
-    factory = qrcode.image.svg.SvgPathImage
+# print(pow(62,30)) 591222134364399413463902591994678504204696392694759424
 
-img = qrcode.make('Some data here', image_factory=factory)
+# print(len(str(pow(2, 33 * 33))))   382 digits long
+
+
+# img = qr.make_image(back_color=(255, 195, 235), fill_color=(55, 95, 35))
+
+# method = 'basic'
+# if method == 'basic':
+#     # Simple factory, just a set of rects.
+#     factory = qrcode.image.svg.SvgImage
+# elif method == 'fragment':
+#     # Fragment factory (also just a set of rects)
+#     factory = qrcode.image.svg.SvgFragmentImage
+# else:
+#     # Combined path factory, fixes white space that may occur when zooming
+#     factory = qrcode.image.svg.SvgPathImage
+#
+# img = qrcode.make('Some data here', image_factory=factory)
