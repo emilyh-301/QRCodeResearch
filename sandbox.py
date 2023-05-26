dda2662b196381754cd4aaf466d35e6e8c9e74f5
emilyh-301 <36495239+emilyh-301@users.noreply.github.com>
2023-05-26 16:07:51 -0400

    edited sandbox.py

--- sandbox.py
+++ sandbox.py
@@ -1,3 +1,5 @@
 import tensorflow as tf
 
-tf.test.is_gpu_available( cuda_only=False, min_cuda_compute_capability=None )
+# tf.test.is_gpu_available( cuda_only=False, min_cuda_compute_capability=None )
+
+print(tf.config.list_physical_devices('GPU'))
