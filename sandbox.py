cfad2572874e23ed1cbe4ae225b15240fd1fd04f
emilyh-301 <36495239+emilyh-301@users.noreply.github.com>
2023-05-28 11:12:40 -0400

    trying things w loss function 5

--- sandbox.py
+++ sandbox.py
@@ -13,6 +13,7 @@ qr1 = qrcode.QRCode(
     border=0,
 )
 qr1.add_data('https://h3turing.vmhost.psu.edu?1234')
+qr1_matrix = list(map(int, qr1.get_matrix()))
 
 # simplest QR Code with no border and box_size of 1
 qr2 = qrcode.QRCode(
@@ -22,8 +23,12 @@ qr2 = qrcode.QRCode(
     border=0,
 )
 qr2.add_data('https://h3turing.vmhost.psu.edu?1234')
+qr2_matrix = list(map(int, qr2.get_matrix()))
+
+print('type of qr matrix')
+print(type(qr1.get_matrix()))
 
 bce = BinaryCrossentropy(from_logits=True)
-loss = bce(qr1.get_matrix().astype(int), qr2.get_matrix().astype(int)).numpy()
+loss = bce(qr1_matrix, qr2_matrix).numpy()
 
 print(loss)
