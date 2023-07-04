3dc5c6b9254ec9ab8f9d5c4521b3f6e9a85d4e0f
emilyh-301 <36495239+emilyh-301@users.noreply.github.com>
2023-07-04 11:17:37 -0400

    try to fix net5, 5

--- models/network5_flatten.py
+++ models/network5_flatten.py
@@ -53,7 +53,7 @@ def _create_model(opt='adadelta', ha='relu', oa='sigmoid', l='mean_squared_error
     model.add(Flatten(input_shape=(33, 33, 1)))
 
     # first layer
-    model.add(Dense(320, activation=hidden_activation, input_shape=(1089, 1)))
+    model.add(Dense(320, activation=hidden_activation))
 
     # second layer
     model.add(Dense(220, activation=hidden_activation))
